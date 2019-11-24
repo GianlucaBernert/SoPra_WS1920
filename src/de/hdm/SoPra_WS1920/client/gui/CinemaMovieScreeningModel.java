d2bfa734ba688718dc6f66b9898bc4e5595df91b
SebastianHermann <sebi.hermann95@t-online.de>
2019-11-24 16:20:12 +0100

    Implemented Admin-Model methods

--- src/de/hdm/SoPra_WS1920/client/gui/CinemaMovieScreeningModel.java
+++ src/de/hdm/SoPra_WS1920/client/gui/CinemaMovieScreeningModel.java
@@ -1,5 +1,7 @@
 package de.hdm.SoPra_WS1920.client.gui;
 
+import java.util.List;
+
 import com.google.gwt.view.client.ListDataProvider;
 
 import de.hdm.SoPra_WS1920.shared.bo.Cinema;
@@ -11,7 +13,7 @@ public class CinemaMovieScreeningModel {
 	Movie movieToShow;
 	Screening screeningToShow;
 	
-	//CinemaForm: cinemaForm
+	//CinemaCard: cinemaForm
 	//MovieCard: movieForm
 	//SreeningCard: screeningForm
 	
@@ -29,4 +31,70 @@ public class CinemaMovieScreeningModel {
 		//initialize Proxy
 	}
 	
+	public void addCinema(Cinema cinema) {
+		cinemaProvider.getList().add(cinema);
+	}
+	
+	public void addMovie(Movie movie) {
+		movieProvider.getList().add(movie);
+	}
+	
+	public void addScreening(Screening screening) {
+		screeningProvider.getList().add(screening);
+	}
+	
+	public void updateCinema(Cinema cinema) {
+		List<Cinema> cinemaList = cinemaProvider.getList();
+		int i = 0;
+		for (Cinema c : cinemaList) {
+			if (c.getId() == cinema.getId()) {
+				cinemaList.set(i, cinema);
+				break;
+			} else {
+				i++;
+			}
+		}
+		cinemaProvider.refresh();
+	}
+	
+	public void updateMovie(Movie movie) {
+		List<Movie> movieList = movieProvider.getList();
+		int i = 0;
+		for (Movie m : movieList) {
+			if (m.getId() == movie.getId()) {
+				movieList.set(i, movie);
+				break;
+			} else {
+				i++;
+			}
+		}
+		movieProvider.refresh();
+	}
+	
+	public void updateScreening(Screening screening) {
+		List<Screening> screeningList = screeningProvider.getList();
+		int i = 0;
+		for (Screening s : screeningList) {
+			if (s.getId() == screening.getId()) {
+				screeningList.set(i, screening);
+				break;
+			} else {
+				i++;
+			}
+		}
+		screeningProvider.refresh();
+	}
+	
+	public void removeCinema(Cinema cinema) {
+		cinemaProvider.getList().remove(cinema);
+	}
+	
+	public void removeMovie(Movie movie) {
+		movieProvider.getList().remove(movie);
+	}
+	
+	public void removeScreening(Screening screening) {
+		screeningProvider.getList().remove(screening);
+	}
+
 }
