220ae8afa2e3260a6efc25530712ee189cd9d8f0
Yesin Soufi <ys018@hdm-stuttgart.de>
2019-11-25 20:16:11 +0100

    Aktualisierung der Dump-Datei_v1

--- MySQL/sopra_ws1920.sql
+++ MySQL/sopra_ws1920.sql
@@ -2,202 +2,232 @@
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='';
 
 -- -----------------------------------------------------
--- Schema sopra_ws1920
+-- Schema popcorns
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema sopra_ws1920
+-- Schema popcorns
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `sopra_ws1920` DEFAULT CHARACTER SET utf8 ;
-USE `sopra_ws1920` ;
+CREATE SCHEMA IF NOT EXISTS `popcorns` DEFAULT CHARACTER SET utf8 ;
+USE `popcorns` ;
 
 -- -----------------------------------------------------
--- Table `sopra_ws1920`.`person`
+-- Table `popcorns`.`BusinessObject`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sopra_ws1920`.`person` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `popcorns`.`BusinessObject` (
+  `bo_id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`bo_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `popcorns`.`BusinessOwnership`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `popcorns`.`BusinessOwnership` (
+  `owner_id` INT NOT NULL AUTO_INCREMENT,
+  `bo_id` INT NOT NULL,
+  PRIMARY KEY (`owner_id`, `bo_id`),
+  INDEX `fk_BusinessOwnership_BusinessObject_idx` (`bo_id` ASC) VISIBLE,
+  CONSTRAINT `fk_BusinessOwnership_BusinessObject`
+    FOREIGN KEY (`bo_id`)
+    REFERENCES `popcorns`.`BusinessObject` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `popcorns`.`Person`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `popcorns`.`Person` (
+  `bo_id` INT NOT NULL AUTO_INCREMENT,
   `firstname` VARCHAR(45) NULL,
   `lastname` VARCHAR(45) NULL,
-  `eMail` VARCHAR(100) NULL,
+  `email` VARCHAR(45) NULL,
   `isAdmin` INT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `sopra_ws1920`.`movie`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sopra_ws1920`.`movie` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `description` LONGTEXT NULL,
-  `genre` VARCHAR(45) NULL,
-  `personFK` INT NOT NULL,
-  PRIMARY KEY (`id`, `personFK`),
-  INDEX `fk_movie_person1_idx` (`personFK` ASC) VISIBLE,
-  CONSTRAINT `fk_movie_person1`
-    FOREIGN KEY (`personFK`)
-    REFERENCES `sopra_ws1920`.`person` (`id`)
+  PRIMARY KEY (`bo_id`),
+  CONSTRAINT `fk_Person_BusinessOwnership1`
+    FOREIGN KEY (`bo_id`)
+    REFERENCES `popcorns`.`BusinessOwnership` (`owner_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `sopra_ws1920`.`group`
+-- Table `popcorns`.`Cinema`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sopra_ws1920`.`group` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `sopra_ws1920`.`survey`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sopra_ws1920`.`survey` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `startDate` DATETIME NULL,
-  `endDate` DATETIME NULL,
-  `groupFK` INT NOT NULL,
-  `personFK` INT NOT NULL,
-  PRIMARY KEY (`id`, `groupFK`, `personFK`),
-  INDEX `fk_survey_group1_idx` (`groupFK` ASC) VISIBLE,
-  INDEX `fk_survey_person1_idx` (`personFK` ASC) VISIBLE,
-  CONSTRAINT `fk_survey_group1`
-    FOREIGN KEY (`groupFK`)
-    REFERENCES `sopra_ws1920`.`group` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_survey_person1`
-    FOREIGN KEY (`personFK`)
-    REFERENCES `sopra_ws1920`.`person` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `sopra_ws1920`.`cinema`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sopra_ws1920`.`cinema` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `popcorns`.`Cinema` (
+  `bo_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   `city` VARCHAR(45) NULL,
   `postCode` VARCHAR(45) NULL,
-  `street` VARCHAR(100) NULL,
+  `street` VARCHAR(45) NULL,
   `streetNo` VARCHAR(45) NULL,
-  `personFK` INT NOT NULL,
-  PRIMARY KEY (`id`, `personFK`),
-  INDEX `fk_cinema_person1_idx` (`personFK` ASC) VISIBLE,
-  CONSTRAINT `fk_cinema_person1`
-    FOREIGN KEY (`personFK`)
-    REFERENCES `sopra_ws1920`.`person` (`id`)
+  `owner_id` INT NOT NULL,
+  PRIMARY KEY (`bo_id`, `owner_id`),
+  INDEX `fk_Cinema_Person1_idx` (`owner_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Cinema_Person1`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `popcorns`.`Person` (`bo_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `sopra_ws1920`.`screening`
+-- Table `popcorns`.`Movie`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sopra_ws1920`.`screening` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `date` DATE NULL,
-  `time` DATETIME NULL,
-  `movieFK` INT NOT NULL,
-  `cinemaFK` INT NOT NULL,
-  `personFK` INT NOT NULL,
-  PRIMARY KEY (`id`, `movieFK`, `cinemaFK`, `personFK`),
-  INDEX `fk_screening_movie_idx` (`movieFK` ASC) VISIBLE,
-  INDEX `fk_screening_cinema1_idx` (`cinemaFK` ASC) VISIBLE,
-  INDEX `fk_screening_person1_idx` (`personFK` ASC) VISIBLE,
-  CONSTRAINT `fk_screening_movie`
-    FOREIGN KEY (`movieFK`)
-    REFERENCES `sopra_ws1920`.`movie` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_screening_cinema1`
-    FOREIGN KEY (`cinemaFK`)
-    REFERENCES `sopra_ws1920`.`cinema` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_screening_person1`
-    FOREIGN KEY (`personFK`)
-    REFERENCES `sopra_ws1920`.`person` (`id`)
+CREATE TABLE IF NOT EXISTS `popcorns`.`Movie` (
+  `bo_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `genre` VARCHAR(45) NULL,
+  `description` LONGTEXT NULL,
+  `owner_id` INT NOT NULL,
+  PRIMARY KEY (`bo_id`, `owner_id`),
+  INDEX `fk_Movie_Person1_idx` (`owner_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Movie_Person1`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `popcorns`.`Person` (`bo_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `sopra_ws1920`.`surveyentry`
+-- Table `popcorns`.`Group`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sopra_ws1920`.`surveyentry` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `screeningFK` INT NOT NULL,
-  `surveyFK` INT NOT NULL,
-  PRIMARY KEY (`id`, `screeningFK`, `surveyFK`),
-  INDEX `fk_surveyentry_screening1_idx` (`screeningFK` ASC) VISIBLE,
-  INDEX `fk_surveyentry_survey1_idx` (`surveyFK` ASC) VISIBLE,
-  CONSTRAINT `fk_surveyentry_screening1`
-    FOREIGN KEY (`screeningFK`)
-    REFERENCES `sopra_ws1920`.`screening` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_surveyentry_survey1`
-    FOREIGN KEY (`surveyFK`)
-    REFERENCES `sopra_ws1920`.`survey` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `popcorns`.`Group` (
+  `bo_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `owner_id` INT NOT NULL,
+  PRIMARY KEY (`bo_id`, `owner_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `sopra_ws1920`.`vote`
+-- Table `popcorns`.`Survey`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sopra_ws1920`.`vote` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `votingWeight` INT NULL,
-  `surveyentryFK` INT NOT NULL,
-  `personFK` INT NOT NULL,
-  PRIMARY KEY (`id`, `surveyentryFK`, `personFK`),
-  INDEX `fk_vote_surveyentry1_idx` (`surveyentryFK` ASC) VISIBLE,
-  INDEX `fk_vote_person1_idx` (`personFK` ASC) VISIBLE,
-  CONSTRAINT `fk_vote_surveyentry1`
-    FOREIGN KEY (`surveyentryFK`)
-    REFERENCES `sopra_ws1920`.`surveyentry` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_vote_person1`
-    FOREIGN KEY (`personFK`)
-    REFERENCES `sopra_ws1920`.`person` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `sopra_ws1920`.`membership`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sopra_ws1920`.`membership` (
+CREATE TABLE IF NOT EXISTS `popcorns`.`Survey` (
+  `bo_id` INT NOT NULL AUTO_INCREMENT,
+  `startDate` DATETIME NULL,
+  `endDate` DATETIME NULL,
+  `owner_id` INT NOT NULL,
   `groupFK` INT NOT NULL,
-  `personFK` INT NOT NULL,
-  PRIMARY KEY (`groupFK`, `personFK`),
-  INDEX `fk_group_has_person_person1_idx` (`personFK` ASC) VISIBLE,
-  INDEX `fk_group_has_person_group1_idx` (`groupFK` ASC) VISIBLE,
-  CONSTRAINT `fk_group_has_person_group1`
-    FOREIGN KEY (`groupFK`)
-    REFERENCES `sopra_ws1920`.`group` (`id`)
+  PRIMARY KEY (`bo_id`, `owner_id`, `groupFK`),
+  INDEX `fk_Survey_Person1_idx` (`owner_id` ASC) VISIBLE,
+  INDEX `fk_Survey_Group1_idx` (`groupFK` ASC) VISIBLE,
+  CONSTRAINT `fk_Survey_Person1`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `popcorns`.`Person` (`bo_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_group_has_person_person1`
-    FOREIGN KEY (`personFK`)
-    REFERENCES `sopra_ws1920`.`person` (`id`)
+  CONSTRAINT `fk_Survey_Group1`
+    FOREIGN KEY (`groupFK`)
+    REFERENCES `popcorns`.`Group` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `popcorns`.`SurveyEntry`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `popcorns`.`SurveyEntry` (
+  `bo_id` INT NOT NULL AUTO_INCREMENT,
+  `surveyFK` INT NOT NULL,
+  PRIMARY KEY (`bo_id`, `surveyFK`),
+  INDEX `fk_SurveyEntry_Survey1_idx` (`surveyFK` ASC) VISIBLE,
+  CONSTRAINT `fk_SurveyEntry_Survey1`
+    FOREIGN KEY (`surveyFK`)
+    REFERENCES `popcorns`.`Survey` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `popcorns`.`Vote`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `popcorns`.`Vote` (
+  `bo_id` INT NOT NULL AUTO_INCREMENT,
+  `votingWeight` INT NULL,
+  `owner_id` INT NOT NULL,
+  `surveyFK` INT NOT NULL,
+  PRIMARY KEY (`bo_id`, `owner_id`, `surveyFK`),
+  INDEX `fk_Vote_Person1_idx` (`owner_id` ASC) VISIBLE,
+  INDEX `fk_Vote_SurveyEntry1_idx` (`surveyFK` ASC) VISIBLE,
+  CONSTRAINT `fk_Vote_Person1`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `popcorns`.`Person` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Vote_SurveyEntry1`
+    FOREIGN KEY (`surveyFK`)
+    REFERENCES `popcorns`.`SurveyEntry` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `popcorns`.`Screening`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `popcorns`.`Screening` (
+  `bo_id` INT NOT NULL AUTO_INCREMENT,
+  `screeningdayTime` DATETIME NULL,
+  `owner_id` INT NOT NULL,
+  `cinemaFK` INT NOT NULL,
+  `movieFK` INT NOT NULL,
+  `surveyEntryFK` INT NOT NULL,
+  PRIMARY KEY (`bo_id`, `owner_id`, `cinemaFK`, `movieFK`, `surveyEntryFK`),
+  INDEX `fk_Screening_Person1_idx` (`owner_id` ASC) VISIBLE,
+  INDEX `fk_Screening_Cinema1_idx` (`cinemaFK` ASC) VISIBLE,
+  INDEX `fk_Screening_Movie1_idx` (`movieFK` ASC) VISIBLE,
+  INDEX `fk_Screening_SurveyEntry1_idx` (`surveyEntryFK` ASC) VISIBLE,
+  CONSTRAINT `fk_Screening_Person1`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `popcorns`.`Person` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Screening_Cinema1`
+    FOREIGN KEY (`cinemaFK`)
+    REFERENCES `popcorns`.`Cinema` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Screening_Movie1`
+    FOREIGN KEY (`movieFK`)
+    REFERENCES `popcorns`.`Movie` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Screening_SurveyEntry1`
+    FOREIGN KEY (`surveyEntryFK`)
+    REFERENCES `popcorns`.`SurveyEntry` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `popcorns`.`Membership`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `popcorns`.`Membership` (
+  `bo_id` INT NOT NULL,
+  `owner_id` INT NOT NULL,
+  PRIMARY KEY (`bo_id`, `owner_id`),
+  INDEX `fk_Person_has_Group_Group1_idx` (`owner_id` ASC) VISIBLE,
+  INDEX `fk_Person_has_Group_Person1_idx` (`bo_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Person_has_Group_Person1`
+    FOREIGN KEY (`bo_id`)
+    REFERENCES `popcorns`.`Person` (`bo_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Person_has_Group_Group1`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `popcorns`.`Group` (`bo_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
